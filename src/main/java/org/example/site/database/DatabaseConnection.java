3a9ce222af5c18f7fa31177b9cb29cef316a98fc
lukas <lukas.balabuch@gmail.com>
2024-05-30 14:00:40 +0200

    zad1

---
+++ src/main/java/org/example/site/database/DatabaseConnection.java
@@ -0,0 +1,35 @@
+package org.example.site.database;
+
+import java.nio.file.Path;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DatabaseConnection
+{
+    private Connection connection;
+    private Connection getConnection()
+    {
+        return connection;
+    }
+
+    public void connect(String path) {
+        try {
+            connection= DriverManager.getConnection(String.format("jdbc:sqlite:%s", path));
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        System.out.println("Connected\n");
+    }
+
+    public void disconnect() {
+        try {
+            if(!(connection != null) && !connection.isClosed() ) {
+                connection.close();
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        System.out.println("Disconnected\n");
+    }
+}
